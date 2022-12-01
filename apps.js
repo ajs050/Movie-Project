a9f6ef3c8e6d0a2b9df2b9713d0d6432bbc1f418
Andrew <ajsantos050@gmail.com>
2022-11-30 19:34:57 -0500

    Added api data into movie list

--- apps.js
+++ apps.js
@@ -1,12 +1,22 @@
+const movieList = document.querySelector(".movie__list")
+const results = document.innerHTML(".movie__search")
 async function onSearchChange(event) {
-    const title = (event.target.value)
+    const title = event.target.value
     const users = await fetch(`http://www.omdbapi.com/?apikey=d5e9a71c&s=${title}`)
     const data = await users.json()
-    const movieList = document.querySelector(".movie__list")
-    movieList.innerHTML = data.map((movie) => `<div class="movie">
-    <figure class="movie__img--wrap">
-      <img src="${movie.poster}">
-    </figure>
-    <p class="movie__title">${movie.title}</p>
-</div>`).join("")
+    movieList.innerHTML = data.Search.map((movie) => userHTML(movie)).join("") 
+} 
+
+
+function userHTML(movie) {
+  return `<div class="movie">
+  <figure class="movie__img--wrap">
+    <img src="${movie.Poster}"> 
+  </figure> 
+  <p class="movie__title">${movie.Title}</p>
+</div>`
+}
+
+function result() {
+
 }
